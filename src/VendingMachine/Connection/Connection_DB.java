c66f6c74583101c8ae0619bf281f726b4c9bac2e
Nodirjon Okilov <okilovnodir456@gmail.com>
2025-03-08 22:58:09 +0900

    changed Connection_DB

--- src/VendingMachine/Connection/Connection_DB.java
+++ src/VendingMachine/Connection/Connection_DB.java
@@ -6,8 +6,8 @@ import java.sql.DriverManager;
 public class Connection_DB {
     public static Connection getConnection() throws Exception {
         String url = "jdbc:postgresql://localhost:5432/postgres";
-        String user = "postgres";
-        String password = "Nodir556";
+        String user = "********";
+        String password = "********";
 
         return DriverManager.getConnection(url, user, password);
     }
